950e8f0a2881fa77bfdfab3d4d6f31fc943f3e63
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-07-12 13:48:40 -0500

    Canny Method with webcam test

--- edge_detection.py
+++ edge_detection.py
@@ -1,2 +1,16 @@
 import cv2 as cv
 import numpy as np
+
+cap = cv2.VideoCapture(0) #connect to webcam
+
+#edge detection
+
+img = cv2.imread('VideoCapture',0)
+edges = cv2.Canny(img,100,200) #Canny Method Algorithm
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
