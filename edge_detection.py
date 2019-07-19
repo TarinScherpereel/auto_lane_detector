6c44265a659b691d7d25b1eaf8cd8153617cae93
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-07-19 14:34:55 -0500

    alex

--- edge_detection.py
+++ edge_detection.py
@@ -3,12 +3,15 @@ import cv2
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
+
+
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
 
 while rval:
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
