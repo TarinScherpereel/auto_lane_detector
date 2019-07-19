89b866a8e97b19da239871b00729de27e9810955
TarinScherpereel <Tarin.scherpereel@ttu.edu>
2019-07-19 17:29:59 -0400

    Works more or less

--- edge_detection.py
+++ edge_detection.py
@@ -6,7 +6,7 @@ capture = cv2.VideoCapture(0)
 while(True):
 
     ret, frame = capture.read()
-
+    blank_frame = np.zeros_like(frame)
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #blur image
@@ -20,22 +20,23 @@ while(True):
     edges = cv2.Canny(canny, 50, 200)
 
     #new
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 200)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100)
 
     #new
     try:
         for line in lines:
             x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            cv2.line(blank_frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
     except TypeError:
         print('Found no lines')
     #cv2.imshow('video gray', grayFrame)
     #cv2.imshow('video blurred', blurred_frame)
-    window = cv2.namedWindow('video canny',CV_WINDOW_AUTOSIZE)
-    cv2.imshow('video canny', canny)
-    cv2.imshow('video original', frame)
+    window = cv2.namedWindow('MainWindow',cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty('MainWindow',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+   # cv2.imshow('video canny', canny)
+   # cv2.imshow('video original', frame)
     #new
-    cv2.imshow("Result Image", frame)
+    cv2.imshow("MainWindow", blank_frame)
     # cv2.imshow('final')
 
 
