6e2fa4ce1f52952ebdd8cb027808e85a19a99c72
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-07-19 15:12:07 -0500

    canny

--- edge_detection.py
+++ edge_detection.py
@@ -11,9 +11,14 @@ while(True):
     #blur image
     blurred_frame = cv2.GaussianBlur(frame, (21,21), 0)
 
+    #canny
+    #edge=cs2.Canny(frame, (25,75))
+    canny = cv2.Canny(blurred_frame, 30, 100)
+
     cv2.imshow('video gray', grayFrame)
     cv2.imshow('video blurred', blurred_frame)
     cv2.imshow('video original', frame)
+    cv2.imshow('video original', canny)
 
     if cv2.waitKey(1) == 27:
         break
