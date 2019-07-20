7758e959c6bafea14032f3452cdea73ce84cc666
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-07-20 17:28:19 -0500

    gray test

--- edge_detection.py
+++ edge_detection.py
@@ -12,35 +12,35 @@ while(True):
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #blur video
-    blurred_frame = cv2.GaussianBlur(grayFrame, (21,21), 0)
+    #blurred_frame = cv2.GaussianBlur(grayFrame, (21,21), 0)
 
     #canny
     #edge=cs2.Canny(frame, (25,75))
 
     #canny method
-    canny = cv2.Canny(blurred_frame, 30, 100)
+    #canny = cv2.Canny(blurred_frame, 30, 100)
 
     #select edges for hough transform
-    edges = cv2.Canny(canny, 50, 200)
+    #edges = cv2.Canny(canny, 50, 200)
 
     #select lines through Hough Transform
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100)
+    #lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100)
 
     #Part of hough transform
-    try:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(blank_frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
-    except TypeError:
-        print('Found no lines')
-    #cv2.imshow('video gray', grayFrame)
+    #try:
+        #for line in lines:
+            #x1, y1, x2, y2 = line[0]
+            #cv2.line(blank_frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    #except TypeError:
+        #print('Found no lines')
+    cv2.imshow('video gray', grayFrame)
     #cv2.imshow('video blurred', blurred_frame)
-    window = cv2.namedWindow('MainWindow',cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty('MainWindow',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    #window = cv2.namedWindow('MainWindow',cv2.WND_PROP_FULLSCREEN)
+    #cv2.setWindowProperty('MainWindow',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
    # cv2.imshow('video canny', canny)
-    #cv2.imshow('video original', frame)
+    cv2.imshow('video original', frame)
     #new
-    cv2.imshow("MainWindow", blank_frame)
+    #cv2.imshow("MainWindow", blank_frame)
     # cv2.imshow('final')
 
 
