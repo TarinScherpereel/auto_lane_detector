b090805b306311508379c7ef133e38d9e6dd2be2
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-07-20 17:49:23 -0500

    back to normal

--- edge_detection.py
+++ edge_detection.py
@@ -18,7 +18,7 @@ while(True):
     #edge=cv2.Canny(frame, (25,75))
 
     #canny method
-    #canny = cv2.Canny(blurred_frame, 30, 100)
+    canny = cv2.Canny(blurred_frame, 30, 100)
 
     #select edges for hough transform
     edges = cv2.Canny(canny, 50, 200)
@@ -27,20 +27,20 @@ while(True):
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100)
 
     #Part of hough transform
-    #try:
-        #for line in lines:
-            #x1, y1, x2, y2 = line[0]
-            #cv2.line(blank_frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
-    #except TypeError:
-        #print('Found no lines')
+    try:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(blank_frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    except TypeError:
+        print('Found no lines')
     #cv2.imshow('video gray', grayFrame)
     #cv2.imshow('video blurred', blurred_frame)
-    #window = cv2.namedWindow('MainWindow',cv2.WND_PROP_FULLSCREEN)
-    #cv2.setWindowProperty('MainWindow',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-    cv2.imshow('video canny', lines)
-    cv2.imshow('video original', frame)
+    window = cv2.namedWindow('MainWindow',cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty('MainWindow',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+    #cv2.imshow('video canny', canny)
+    #cv2.imshow('video original', frame)
     #new
-    #cv2.imshow("MainWindow", blank_frame)
+    cv2.imshow("MainWindow", blank_frame)
     # cv2.imshow('final')
 
 
