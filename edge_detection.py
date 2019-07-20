a47a6421ba48de23ab654a0ba59f162304173d66
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-07-20 17:38:06 -0500

    test canny not black

--- edge_detection.py
+++ edge_detection.py
@@ -15,13 +15,13 @@ while(True):
     blurred_frame = cv2.GaussianBlur(grayFrame, (21,21), 0)
 
     #canny
-    #edge=cs2.Canny(frame, (25,75))
+    edge=cs2.Canny(frame, (25,75))
 
     #canny method
     canny = cv2.Canny(blurred_frame, 30, 100)
 
     #select edges for hough transform
-    edges = cv2.Canny(canny, 50, 200)
+    #edges = cv2.Canny(canny, 50, 200)
 
     #select lines through Hough Transform
     #lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100)
