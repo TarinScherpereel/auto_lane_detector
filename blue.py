0d1f023cc1cc6f6dd7ab5873ea29021023cb0e9c
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-08-03 14:37:25 -0500

    back

--- blue.py
+++ blue.py
@@ -21,7 +21,7 @@ while(1):
 
     edges = cv2.Canny(mask, 200, 400)
 
-        return edges
+return edges
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
