c8f054b7736cf45cf4ddf5ccf186727f31db1fcf
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-08-07 14:39:43 -0500

    d

--- roverback.py
+++ roverback.py
@@ -45,13 +45,8 @@ def forward():
 
 #Stop
 def stop():
-    GPIO.output(Motor1, GPIO.HIGH)
-    GPIO.output(Motor2, GPIO.HIGH)
-    GPIO.output(EnableA, GPIO.LOW)
-
-    GPIO.output(Motor3, GPIO.HIGH)
-    GPIO.output(Motor4, GPIO.HIGH)
-    GPIO.output(EnableB, GPIO.LOW)
+    GPIO.output(EnableA, GPIO.HIGH)
+    GPIO.output(EnableB, GPIO.HIGH)
 
 
 
