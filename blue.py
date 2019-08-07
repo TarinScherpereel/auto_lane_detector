4d3289979d2276bb40cf68e282918da40ca73215
Tarin Scherpereel <tarin.scherpereel@Tarins-MacBook-Air.local>
2019-08-07 13:27:37 -0500

    b

--- blue.py
+++ blue.py
@@ -8,13 +8,13 @@ while True:
     blur = cv2.GaussianBlur(frame, (7,7), 0)#GaussianBlur
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)#HSV shit
 ########################################################################################
-    #HSV shit
+    #HSV
     LowerBlue = np.array ([30,40, 0])
     UpperBlue = np.array([150, 255, 255])
     mask = cv2.inRange(hsv, LowerBlue, UpperBlue)
 ########################################################################################
     #Hough Stuff
-    edges = cv2.Canny(blur,100,100)#canny shit
+    edges = cv2.Canny(blur,100,100)#canny
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100)#lines
     for line in lines:
         x1, y1, x2, y2 = line[0]
@@ -26,11 +26,12 @@ while True:
     cv2.rectangle(frame, (630,470), (0,200), (0,255,0),5)#creates rectangle
 ########################################################################################
 
-    #cv2.imshow('orignal with rectangle',frame)#showing normal frame
-    #cv2.imshow('Gaussian Blur', blur)#blur
-    #cv2.imshow('Edges', edges)#edges
-    #cv2.imshow('hsv', hsv)#hsv
-    #cv2.imshow('mask', mask)#masking
+    cv2.imshow('orignal with rectangle',frame)#showing normal frame
+    cv2.imshow('Gaussian Blur', blur)#blur
+    cv2.imshow('Edges', edges)#edges
+    cv2.imshow('hsv', hsv)#hsv
+    cv2.imshow('mask', mask)#masking
+
     cv2.imshow('result', res)#views result
     #How to stop the video
     if cv2.waitKey(1) & 0xFF==ord('q'): #pressing "q" on keyboard will
